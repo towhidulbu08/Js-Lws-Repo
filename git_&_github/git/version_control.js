f186c069e0d9dcd39d5c362001acdfcfd8d39005
towhidulbu08 <towhidulislam26121997@gmail.com>
2024-05-12 16:33:12 +0500

    fourth commt

--- git_&_github/git/version_control.js
+++ git_&_github/git/version_control.js
@@ -1,1 +1,13 @@
-// version control
+/**
+ * Git Is  a version control Tool.It controls the versions of file system.It tracks the working directory and keep tracks of every versions of a file.
+ * 1. Working directory(current folder in which i am working now)
+ * 2. Local repository
+ * 3. Remote repository
+ *
+ * Git Configuration and Command=> 1.git config --list
+ *                     2.git config --global user.name
+ *                     3. pwd
+ *                     4. touch file name (create a file in the current directory)
+ *                     5. mkdir folder name(create a folder )
+ *
+ * */
